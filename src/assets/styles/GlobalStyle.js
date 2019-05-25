9b644070358e8b6b638471f9a4b27b9df880cf0b
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-25 12:33:58 +0200

    create GlobalStyle

---
+++ src/assets/styles/GlobalStyle.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+   @import url(https://fonts.googleapis.com/css?family=Open+Sans:300,regular,600,800);
+
+   body {
+      margin: 0;
+      padding: 0;
+      font-family: "Open Sans";
+   }
+
+   html{
+      font-size: 62.5%;
+   }
+
+   *, *::before, *::after {
+      box-sizing: border-box;
+   }
+`;
+
+export default GlobalStyle;
