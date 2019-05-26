655ee78a872c7909c23dc8eeaf5cf141682eccbc
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-26 11:28:12 +0200

    fix quotes

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import styled from "styled-components";
-import PageTemplate from "templates/PageTemplate";
+import React from 'react';
+import styled from 'styled-components';
+import PageTemplate from 'templates/PageTemplate';
 
 const H1 = styled.h1`
   font-size: 70px;
