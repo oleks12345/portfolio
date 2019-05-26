6a97e7ced3c5b3ef94dc062efe9fa9ed0febe979
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-26 13:50:10 +0200

    a create Nav

--- src/components/Menu/Nav.js
+++ src/components/Menu/Nav.js
@@ -1,13 +1,54 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 // import styled from "styled-components";
-import Hamburger from "components/Menu/Hamburger";
+import Hamburger from 'components/Menu/Hamburger';
+import Menu from 'components/Menu/Menu';
+import IconGh from 'assets/images/github.svg';
+import IconFb from 'assets/images/facebook.svg';
+import IconIn from 'assets/images/linkedin.svg';
+
+const menuItems = [
+   {
+      name: 'Home',
+      id: '#header'
+   },
+   {
+      name: 'placeaholder',
+      id: '#1'
+   },
+   {
+      name: 'placeaholder',
+      id: '#2'
+   },
+   {
+      name: 'placeaholder',
+      id: '#3'
+   }
+];
+const socials = [
+   {
+      icon: IconFb,
+      name: 'facebook',
+      link: ''
+   },
+   {
+      icon: IconGh,
+      name: 'github',
+      link: ''
+   },
+   {
+      icon: IconIn,
+      name: 'linkedin',
+      link: ''
+   }
+];
 
 const Nav = () => {
-   const [ isMenuOpen, setIsMenuOpen ] = useState( false );
+   const [ isMenuOpen, setIsMenuOpen ] = useState( true );
    const handleMenuClick = () => setIsMenuOpen( !isMenuOpen );
    return (
     <>
       <Hamburger onClick={ handleMenuClick } isOpen={ isMenuOpen } />
+      <Menu menuItems={ menuItems } isOpen={ isMenuOpen } socials={ socials } />
     </>
    );
 };
