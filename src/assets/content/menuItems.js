017c87203ea19fc0ff6ae69bd3063638a18db5b0
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-26 14:04:36 +0200

    move menuItems to separate file

---
+++ src/assets/content/menuItems.js
@@ -0,0 +1,18 @@
+export const menuItems = [
+   {
+      name: 'Home',
+      id: '#header'
+   },
+   {
+      name: 'placeaholder',
+      id: '#1'
+   },
+   {
+      name: 'placeaholder',
+      id: '#2'
+   },
+   {
+      name: 'placeaholder',
+      id: '#3'
+   }
+];
--- src/components/Menu/Nav.js
+++ src/components/Menu/Nav.js
@@ -2,28 +2,11 @@ import React, { useState } from 'react';
 // import styled from "styled-components";
 import Hamburger from 'components/Menu/Hamburger';
 import Menu from 'components/Menu/Menu';
+import { menuItems } from 'assets/content/menuItems';
 import IconGh from 'assets/images/github.svg';
 import IconFb from 'assets/images/facebook.svg';
 import IconIn from 'assets/images/linkedin.svg';
 
-const menuItems = [
-   {
-      name: 'Home',
-      id: '#header'
-   },
-   {
-      name: 'placeaholder',
-      id: '#1'
-   },
-   {
-      name: 'placeaholder',
-      id: '#2'
-   },
-   {
-      name: 'placeaholder',
-      id: '#3'
-   }
-];
 const socials = [
    {
       icon: IconFb,
@@ -43,7 +26,7 @@ const socials = [
 ];
 
 const Nav = () => {
-   const [ isMenuOpen, setIsMenuOpen ] = useState( true );
+   const [ isMenuOpen, setIsMenuOpen ] = useState( false );
    const handleMenuClick = () => setIsMenuOpen( !isMenuOpen );
    return (
     <>
