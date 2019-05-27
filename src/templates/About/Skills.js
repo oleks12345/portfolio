7fb4fa7ddb7dfcd1c04215a03d54a6be48115c04
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-27 15:51:08 +0200

    create placeholder skills component

---
+++ src/templates/About/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Skill from 'components/Skill/Skill';
+
+const Skills = () => {
+   return (
+      <div>
+         <Skill />
+      </div>
+   );
+};
+
+export default Skills;
