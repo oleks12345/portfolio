082164f6c8e16cf335dc6378c641a759df924d16
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-27 15:50:34 +0200

    create placeholder Skill component

---
+++ src/components/Skill/Skill.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import placeholder from 'assets/images/placeholder.svg';
+
+const Skill = () => {
+   return (
+      <div>
+         <img src={ placeholder } alt="" />
+         <div>PlaceHolder</div>
+      </div>
+   );
+};
+
+export default Skill;
