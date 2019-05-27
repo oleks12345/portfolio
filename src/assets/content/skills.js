f9b419fc158b0dedf899a872d9113edb389757cc
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-27 15:51:29 +0200

    create placeholder skills content file

---
+++ src/assets/content/skills.js
@@ -0,0 +1,28 @@
+const skills = {};
+skills.skills = {
+   name: 'Umiejętności',
+   content: [
+      {
+         icon: '',
+         name: '',
+      },
+   ],
+};
+skills.tolls = {
+   name: 'Narzedzia',
+   content: [
+      {
+         icon: '',
+         name: '',
+      },
+   ],
+};
+skills.learning = {
+   name: 'Aktualnie uczę się',
+   content: [
+      {
+         icon: '',
+         name: '',
+      },
+   ],
+};
