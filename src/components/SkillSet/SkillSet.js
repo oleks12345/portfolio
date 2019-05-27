d652bf8cd21133d58ce77a02b5cbbed6d3a6304d
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-27 16:44:01 +0200

    change SkillsList style

--- src/components/SkillSet/SkillSet.js
+++ src/components/SkillSet/SkillSet.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import Skill from 'components/Skill/Skill';
 
 const SkillSetHeader = styled.h3`
    text-align: center;
    font-size: ${( { theme } ) => theme.font.size.m};
 `;
-
+const SkillsList = styled.ul`
+   padding: 10px;
+   list-style: none;
+   display: flex;
+   flex-wrap: wrap;
+`;
+const createSkillsList = ( skillList ) =>
+   skillList.map( ( skill ) => <Skill key={ skill.name } skill={ skill } /> );
 const SkillSet = ( { skillSet } ) => {
    return (
       <div>
          <SkillSetHeader>{skillSet.name}</SkillSetHeader>
+         <SkillsList>{createSkillsList( skillSet.content )}</SkillsList>
       </div>
    );
 };
