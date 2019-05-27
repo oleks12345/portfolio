27b83c229146c6be6e6d488f80b0ee0cebde4906
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-27 17:12:18 +0200

    style SkillsList

--- src/components/SkillSet/SkillSet.js
+++ src/components/SkillSet/SkillSet.js
@@ -12,6 +12,7 @@ const SkillsList = styled.ul`
    list-style: none;
    display: flex;
    flex-wrap: wrap;
+   justify-content: space-between;
 `;
 const createSkillsList = ( skillList ) =>
    skillList.map( ( skill ) => <Skill key={ skill.name } skill={ skill } /> );
