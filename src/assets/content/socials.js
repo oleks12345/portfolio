9a903e01500222eb9b74ebf896695fa49ba266ca
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-28 12:35:19 +0200

    add links

--- src/assets/content/socials.js
+++ src/assets/content/socials.js
@@ -1,21 +1,21 @@
 import IconGh from 'assets/images/socials/github.svg';
-import IconFb from 'assets/images/socials/facebook.svg';
+// import IconFb from 'assets/images/socials/facebook.svg';
 import IconIn from 'assets/images/socials/linkedin.svg';
 
 export const socials = [
-   {
-      icon: IconFb,
-      name: 'facebook',
-      link: '',
-   },
+   // {
+   //    icon: IconFb,
+   //    name: 'facebook',
+   //    link: '',
+   // },
    {
       icon: IconGh,
       name: 'github',
-      link: '',
+      link: 'https://github.com/oleks12345/',
    },
    {
       icon: IconIn,
       name: 'linkedin',
-      link: '',
+      link: 'https://www.linkedin.com/in/aleksander-uniatowicz-175650153/',
    },
 ];
