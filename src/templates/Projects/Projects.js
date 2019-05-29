972c1ca248f3deadbc8a189b092373b4b49854c0
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-29 14:20:01 +0200

    add and style  Projects title

--- src/templates/Projects/Projects.js
+++ src/templates/Projects/Projects.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+import { ColorSecondary } from 'components/ColorSecondary/ColorSecondary';
+
+const ProjectsSection = styled.section`
+   color: ${( { theme } ) => theme.grey};
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   padding: 50px 10%;
+`;
+const Title = styled.h2`
+   font-size: ${( { theme } ) => theme.font.size.l};
+   text-transform: uppercase;
+`;
 
 const Projects = () => {
-   return <div>Projects</div>;
+   return (
+      <ProjectsSection>
+         <Title>
+            Moje <ColorSecondary>Projekty</ColorSecondary>
+         </Title>
+      </ProjectsSection>
+   );
 };
 
 export default Projects;
