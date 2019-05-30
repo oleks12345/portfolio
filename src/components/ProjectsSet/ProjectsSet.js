3ba21fc0dc5a25b4f300788f78971e3f4b3520ad
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-30 12:52:20 +0200

    create ProjectsSet component

---
+++ src/components/ProjectsSet/ProjectsSet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Project from 'components/Project/Project';
+
+const ProjectsSet = () => {
+   return (
+      <div>
+         <h2>Title</h2>
+         <Project />
+      </div>
+   );
+};
+
+export default ProjectsSet;
