a1d5119c42f4bb41de0f9bf5a4bb492945916804
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-30 16:50:40 +0200

    style project

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -2,16 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+const ProjectWrapper = styled.div`
+   width: 100%;
+
+   font-size: ${( { theme } ) => theme.font.size.s};
+   ${( { theme } ) => theme.mdq.md} {
+      width: ${( { isBig } ) => ( isBig ? '100%' : '50%' )};
+   }
+`;
 const ProjectTitle = styled.h2`
+   text-align: center;
    font-weight: ${( { theme } ) => theme.font.weight.regular};
+`;
+const ProjectImage = styled.div`
+   height: 500px;
+   max-height: 50vw;
+   width: 100%;
+   background: center/contain no-repeat url(${( { src } ) => src}),
+      ${( { theme } ) => theme.greyTransparent};
+`;
+
+const ProjectDescription = styled.p`
+   width: ${( { isBig } ) => ( isBig ? '50%' : '100%' )};
 `;
 
 const Project = ( { project } ) => {
    return (
-      <div>
+      <ProjectWrapper isBig={ project.isBig }>
+         <ProjectImage src={ project.img } alt="" />
+
          <ProjectTitle> {project.title}</ProjectTitle>
-         <img src={ project.img } alt="" />
-      </div>
+         <ProjectDescription>
+            {project.desc.split( '<br>' ).map( ( text ) => (
+               <>
+                  {text}
+                  <br />
+               </>
+            ) )}
+         </ProjectDescription>
+      </ProjectWrapper>
    );
 };
 Project.propTypes = {
