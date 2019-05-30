5925caf7c1c7468761924c97125cf975ee34d43e
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-30 13:58:00 +0200

    add and use props

--- src/components/ProjectsSet/ProjectsSet.js
+++ src/components/ProjectsSet/ProjectsSet.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 import Project from 'components/Project/Project';
 
 const Title = styled.h2`
    font-size: ${( props ) => props.theme.font.size.m};
 `;
+const createProjectsList = ( projectsList ) =>
+   projectsList.map( ( project ) => (
+      <Project key={ project.title } project={ project } />
+   ) );
 
-const ProjectsSet = () => {
+const ProjectsSet = ( { title, projectsList } ) => {
    return (
       <div>
-         <Title>Komercyjne</Title>
-         <Project />
+         <Title>{title}</Title>
+         {createProjectsList( projectsList )}
       </div>
    );
 };
 
+ProjectsSet.propTypes = {
+   title: PropTypes.string.isRequired,
+   projectsList: PropTypes.arrayOf(
+      PropTypes.shape( {
+         title: PropTypes.string.isRequired,
+         img: PropTypes.string.isRequired,
+         desc: PropTypes.string.isRequired,
+         isbig: PropTypes.bool,
+      } )
+   ).isRequired,
+};
 export default ProjectsSet;
