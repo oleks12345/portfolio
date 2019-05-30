cac7ce879baa7fa32fd263bfe5149feb7d3daf9e
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-30 12:52:03 +0200

    create Project component

---
+++ src/components/Project/Project.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import placeholder from 'assets/images/projects/placeholder.svg';
+
+const Project = () => {
+   return (
+      <div>
+         <img src={ placeholder } alt="" />
+      </div>
+   );
+};
+
+export default Project;
