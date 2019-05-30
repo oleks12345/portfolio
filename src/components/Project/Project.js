0439f0c02b0abc38e8e5d52e9eb918c172a03f13
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-30 13:57:41 +0200

    add and use props

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import placeholder from 'assets/images/projects/placeholder.svg';
 
 const ProjectTitle = styled.h2`
    font-weight: ${( { theme } ) => theme.font.weight.regular};
 `;
 
-const Project = () => {
+const Project = ( { project } ) => {
    return (
       <div>
-         <ProjectTitle> Placeholder title </ProjectTitle>
-         <img src={ placeholder } alt="" />
+         <ProjectTitle> {project.title}</ProjectTitle>
+         <img src={ project.img } alt="" />
       </div>
    );
 };
-
+Project.propTypes = {
+   project: PropTypes.shape( {
+      title: PropTypes.string.isRequired,
+      img: PropTypes.string.isRequired,
+      desc: PropTypes.string.isRequired,
+      isbig: PropTypes.bool,
+   } ).isRequired,
+};
 export default Project;
