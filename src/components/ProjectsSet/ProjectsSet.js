112f0639465ef706229aafd7ab808bdf8bac1207
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-30 13:02:00 +0200

    style title

--- src/components/ProjectsSet/ProjectsSet.js
+++ src/components/ProjectsSet/ProjectsSet.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 import Project from 'components/Project/Project';
+
+const Title = styled.h2`
+   font-size: ${( props ) => props.theme.font.size.m};
+`;
 
 const ProjectsSet = () => {
    return (
       <div>
-         <h2>Title</h2>
+         <Title>Komercyjne</Title>
          <Project />
       </div>
    );
