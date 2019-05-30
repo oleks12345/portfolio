eeb6910d4b3f1fdfa232db4c190d89cbcdd622f3
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-30 13:39:04 +0200

    fix projects prlaceholder

---
+++ src/assets/content/projects.js
@@ -0,0 +1,22 @@
+import placeholder from 'assets/images/projects/placeholder.svg';
+
+export const projects = [
+   {
+      category: 'Komercyjne',
+      projectsList: [
+         {
+            title: 'Placeholder Title',
+            img: placeholder,
+            desc:
+               'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas commodi totam et rerum aspernatur? Voluptates?',
+            isBig: true,
+         },
+         {
+            title: 'not big Title',
+            img: placeholder,
+            desc:
+               'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas commodi totam et rerum aspernatur? Voluptates?',
+         },
+      ],
+   },
+];
