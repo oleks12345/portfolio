be025f15e9303c844ea629d931142ba09725710c
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-30 12:26:44 +0200

    add skils.used

--- src/assets/content/skills.js
+++ src/assets/content/skills.js
@@ -3,6 +3,7 @@ import css from 'assets/images/skills/css-3.svg';
 import javascript from 'assets/images/skills/javascript.svg';
 import react from 'assets/images/skills/react.svg';
 import git from 'assets/images/skills/git-icon.svg';
+
 import bootstrap from 'assets/images/skills/bootstrap-solid.svg';
 import github from 'assets/images/skills/github-icon.svg';
 import sass from 'assets/images/skills/sass.svg';
@@ -13,9 +14,14 @@ import babel from 'assets/images/skills/babel.svg';
 import eslint from 'assets/images/skills/eslint.svg';
 import prettier from 'assets/images/skills/prettier.svg';
 import npm from 'assets/images/skills/npm.svg';
+
 import express from 'assets/images/skills/express.svg';
 import nodejs from 'assets/images/skills/nodejs-icon.svg';
 import mongodb from 'assets/images/skills/mongodb.svg';
+
+import nginx from 'assets/images/skills/nginx.svg';
+import jenkins from 'assets/images/skills/jenkins.svg';
+import apache from 'assets/images/skills/apache.svg';
 
 export const skills = {};
 skills.skills = {
@@ -102,6 +108,23 @@ skills.learning = {
       {
          icon: mongodb,
          name: 'mongoDB',
+      },
+   ],
+};
+skills.used = {
+   name: 'Miałem okazję kożystać z',
+   content: [
+      {
+         icon: apache,
+         name: 'apache web server',
+      },
+      {
+         icon: jenkins,
+         name: 'jenkins cI',
+      },
+      {
+         icon: nginx,
+         name: 'nginx web server',
       },
    ],
 };
