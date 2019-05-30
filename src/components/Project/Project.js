c048ab0cbf73d8735a2a2c41d3ece1fa2236e27c
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-30 13:40:29 +0200

    style Project Title

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 import placeholder from 'assets/images/projects/placeholder.svg';
+
+const ProjectTitle = styled.h2`
+   font-weight: ${( { theme } ) => theme.font.weight.regular};
+`;
 
 const Project = () => {
    return (
       <div>
+         <ProjectTitle> Placeholder title </ProjectTitle>
          <img src={ placeholder } alt="" />
       </div>
    );
