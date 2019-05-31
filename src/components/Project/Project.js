ffc86e4732a148287d7b11d5466e400be3cbe4e6
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-05-31 18:22:37 +0200

    style Project

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import githubIcon from 'assets/images/projects/links/github.svg';
+import liveIcon from 'assets/images/projects/links/live.svg';
 
 const ProjectWrapper = styled.div`
    width: 100%;
-
+   padding: 10px;
    font-size: ${( { theme } ) => theme.font.size.s};
    ${( { theme } ) => theme.mdq.lg} {
       width: ${( { isBig } ) => ( isBig ? '100%' : '50%' )};
@@ -21,25 +23,80 @@ const ProjectImage = styled.div`
    background: center/contain no-repeat url(${( { src } ) => src}),
       ${( { theme } ) => theme.greyTransparent};
 `;
-
+const ProjectAbout = styled.div`
+   display: flex;
+   flex-wrap: wrap;
+   align-items: center;
+`;
+const ProjectLinks = styled.div`
+   flex-grow: 1;
+   display: flex;
+   flex-wrap: wrap;
+   min-width: 250px;
+`;
 const ProjectDescription = styled.p`
-   width: ${( { isBig } ) => ( isBig ? '50%' : '100%' )};
+   width: 100%;
+   ${( { theme } ) => theme.mdq.lg} {
+      width: ${( { isBig } ) => ( isBig ? '50%' : '100%' )};
+   }
+`;
+const ProjectLink = styled.a`
+   position: relative;
+   flex-grow: 1;
+   width: 50%;
+   min-width: 200px;
+   max-width: 400px;
+   height: 50px;
+   padding: 5px;
+   font-weight: ${( { theme } ) => theme.font.weight.semibold};
+   color: white;
+   text-decoration: none;
+   background-color: ${( { gh, lv, theme } ) => {
+      if ( gh ) return theme.grey;
+      if ( lv ) return theme.secondary;
+   }};
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   ${( { theme } ) => theme.mdq.lg} {
+      max-width: none;
+   }
+`;
+const ProjectLinkImg = styled.img`
+   height: 100%;
+
+   margin-right: 10px;
 `;
 
 const Project = ( { project } ) => {
    return (
       <ProjectWrapper isBig={ project.isBig }>
          <ProjectImage src={ project.img } alt="" />
-
          <ProjectTitle> {project.title}</ProjectTitle>
-         <ProjectDescription>
-            {project.desc.split( '<br>' ).map( ( text ) => (
-               <>
-                  {text}
-                  <br />
-               </>
-            ) )}
-         </ProjectDescription>
+         <ProjectAbout>
+            <ProjectDescription isBig={ project.isBig }>
+               {project.desc.split( '<br>' ).map( ( text ) => (
+                  <React.Fragment key={ text }>
+                     {text}
+                     <br />
+                  </React.Fragment>
+               ) )}
+            </ProjectDescription>
+            <ProjectLinks>
+               {project.links.githubLink && (
+                  <ProjectLink gh href={ project.links.githubLink }>
+                     <ProjectLinkImg src={ githubIcon } alt="" />
+                     Github
+                  </ProjectLink>
+               )}
+               {project.links.liveLink && (
+                  <ProjectLink lv href={ project.links.liveLink }>
+                     <ProjectLinkImg src={ liveIcon } alt="Live" />
+                     Live
+                  </ProjectLink>
+               )}
+            </ProjectLinks>
+         </ProjectAbout>
       </ProjectWrapper>
    );
 };
