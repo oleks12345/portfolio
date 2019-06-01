e108cd0ed687bae74574efd2f32106276240a98a
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-01 11:16:07 +0200

    add Contact to app.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -5,6 +5,7 @@ import Header from 'templates/Header/Header';
 import About from 'templates/About/About';
 import { Spacer } from 'components/Spacer/Spacer';
 import Projects from 'templates/Projects/Projects';
+import Contact from 'templates/Contact/Contact';
 
 const Placeholder = styled.div`
    height: 100vh;
@@ -19,6 +20,7 @@ const App = () => {
          <Spacer />
          <Projects />
          <Spacer rotate />
+         <Contact />
          <Placeholder />
       </PageTemplate>
    );
