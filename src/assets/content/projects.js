d1a54340d42b84a173b9b8df86b84b52a94402c3
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-24 12:42:10 +0200

    add newsWebsite to projects

--- src/assets/content/projects.js
+++ src/assets/content/projects.js
@@ -1,6 +1,7 @@
 import canis from 'assets/images/projects/canis.webp';
 import mache from 'assets/images/projects/mache.webp';
 import portfolio from 'assets/images/projects/portfolio.webp';
+import newsWebsite from 'assets/images/projects/newsWebsite.webp';
 // import placeholder from 'assets/images/projects/placeholder.svg';
 
 export const projects = [
@@ -24,6 +25,21 @@ export const projects = [
    {
       category: 'Projekty Własne',
       projectsList: [
+         {
+            title: 'newsWebsite',
+            img: newsWebsite,
+            desc: `Strona wyświetlająca artykuły z polskich serwisów informacyjnych pobrane z zewnętrznego API ( newsAPI.org ) 
+               <br>
+               Wykorzystane technologie: React, styled-components, react-router-dom 
+               <br>
+               Wykorzystane narzędzia: npm, eslint, prettier, create-react-app
+               <br>
+               strona nadal w budowie, planowane dodanie innych podstron.`,
+            links: {
+               githubLink: 'https://github.com/oleks12345/newsWebsite',
+               liveLink: 'https://oleks.pl/news',
+            },
+         },
          {
             title: 'Mache',
             img: mache,
